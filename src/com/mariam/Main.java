fb388dddfb57645ff88eca805c16c3a63a591390
Mariyam-bs23 <mariyam.begom@bs-23.com>
2023-08-31 20:07:09 +0600

    multi-demensional array added

--- src/com/mariam/Main.java
+++ src/com/mariam/Main.java
@@ -20,10 +20,15 @@ public class Main {
     }
 }
 
-// array practise
+// array class
 class Array {
     public static void main(String[] args) {
+        // 1D array
         int[] a = {1,2,3,4};
         System.out.println("array a " + Arrays.toString(a));
+
+        //multi-dimensional array
+        int[][] numbers = {{1,2,3},{4,5,6}};
+        System.out.println("multi-dimensional array " + Arrays.deepToString(numbers));
     }
 }
