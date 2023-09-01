0d5291391e2e0659c9daece143cde027abb1330c
Mariyam-bs23 <mariyam.begom@bs-23.com>
2023-09-01 17:41:10 +0600

    developed mortgage calculator

--- src/com/mariam/Main.java
+++ src/com/mariam/Main.java
@@ -1,6 +1,7 @@
 package com.mariam;
 
 import java.awt.*;
+import java.text.NumberFormat;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -45,5 +46,28 @@ class ReadInput {
         System.out.print("Age : ");
         byte age = scanner.nextByte();
         System.out.println("Your age is " + age);
+    }
+}
+
+//mortgage calculator
+class MortgageCalculator{
+    public static void main(String[] args){
+        byte month_in_years = 12;
+        
+        //taking inputs
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Principal : ");
+        int principal = scanner.nextInt();
+
+        System.out.print("Annual interest rate : ");
+        float annualInterestRate = scanner.nextFloat();
+        float monthlyInterest = annualInterestRate / month_in_years / 100;
+
+        System.out.print("Period (Years) : ");
+        byte period = scanner.nextByte();
+        int no_of_payments = period*12;
+        double mortgage = principal * (monthlyInterest * Math.pow(1 + monthlyInterest , no_of_payments)) / (Math.pow(1 + monthlyInterest , no_of_payments) - 1);
+
+        System.out.println("mortgage "+ NumberFormat.getCurrencyInstance().format(mortgage));
     }
 }
