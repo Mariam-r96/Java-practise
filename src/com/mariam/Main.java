5c0a676fc7463868eb08f2cc051ad19446c9dd9c
Mariyam-bs23 <mariyam.begom@bs-23.com>
2023-09-20 18:36:11 +0600

    merged all the branches

--- src/com/mariam/Main.java
+++ src/com/mariam/Main.java
@@ -8,7 +8,15 @@ import java.util.Scanner;
 // The public class that has the same name as the Java file.
 public class Main {
     public static void main(String[] args) {
-
+        //        Linked list
+        var list = new LinkedList();
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+        list.addFirst(90);
+        System.out.println(list.indexOf(20));
+        System.out.println(list.contains(200));
+        list.removeLast();
     }
 }
 
